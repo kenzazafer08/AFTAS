43d6fa08653dc76a8f129d75dd97ac2115dea7b6
unknown <zaaferkenza08@gmail.com>
2023-12-12 10:59:58 +0100

    generate home components

--- Aftas-frontend/src/app/app.module.ts
+++ Aftas-frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/utils/navbar/navbar.component';
+import { HeaderComponent } from './components/home/header/header.component';
+import { HomeComponent } from './components/home/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
